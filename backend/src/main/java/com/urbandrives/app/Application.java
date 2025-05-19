a1ea3deb1fdccde33e1108adb53fdbbceee3348f
kelvinwambua <shwiftykelvin@gmail.com>
2025-05-19 11:34:40 +0300

    DB Connection

--- backend/src/main/java/com/urbandrives/app/Application.java
+++ backend/src/main/java/com/urbandrives/app/Application.java
@@ -1,11 +1,26 @@
 package com.urbandrives.app;
 
+import com.urbandrives.app.config.DatabaseConnection;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.sql.Connection;
 
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        try {
+
+            Connection conn = DatabaseConnection.getConnection();
+            System.out.println("Database connection successful!");
+            SpringApplication.run(Application.class, args);
+
+        } catch (Exception e) {
+            System.out.println("Database connection failed: " + e.getMessage());
+            e.printStackTrace();
+        }
     }
-}
+
+
+    }
+
